17659813ae1a52f27dd45d0b3075502b45d5d738
Vikas-Gupta <gupta-vikas@hotmail.com>
2020-06-06 17:57:53 +0530

    Add Implementation added so that our Rule Manager can hold any newly added rule to execute it.

--- Business/Interfaces/IRuleManager.cs
+++ Business/Interfaces/IRuleManager.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Shared.Entities.Interface;
 
 namespace Business.Interfaces
 {
     public interface IRuleManager
     {
         List<string> ExecuteRules();
+
+        void AddRule(IRule rule);
     }
 }
--- Business/Managers/RuleManager.cs
+++ Business/Managers/RuleManager.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Business.Interfaces;
+using Shared.Entities.Interface;
 
 namespace Business.Managers
 {
     public class RuleManager : IRuleManager
     {
+        List<IRule> _rules = new List<IRule>();
+
+        public RuleManager()
+        {
+
+        }
+
         public List<string> ExecuteRules()
         {
             throw new NotImplementedException();
+        }
+
+
+        public void AddRule(IRule rule)
+        {
+            _rules.Add(rule);
         }
     }
 }
