0bd48e23326dc1ddeb3a564d153277b326d4d190
Vikas-Gupta <gupta-vikas@hotmail.com>
2020-06-06 17:08:34 +0530

    Add Rule Engine Project and Test Project with One test case that fails in compliation.

---
+++ RuleEngine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RuleEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ RuleEngineTest/TestAll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RuleEngineTest
+{
+    [TestClass]
+    public class TestAll
+    {
+        [TestMethod]
+        public void Add_Physical_Product_Rule()
+        {
+            Rule rule = new Rule();
+            Assert.IsTrue(RuleManager.AddRule(rule));
+        }
+    }
+}
